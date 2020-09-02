cf1e3e9db3e400c9e7c70d89ca9ad8321797faf5
alexshelto <alexshelto@icloud.com>
2020-09-02 11:34:52 -0400

    Fixed prop usage

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//App.js
+// container holding both input window and output window
+//
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+import React from 'react';
+import './App.css';
+import InputWindow from './InputWindow';
+import OutputWindow from './OutputWindow';
+
+
+
+class App extends React.Component {
+  constructor(props){
+    super(props);
+
+    this.onInputChange = this.onInputChange.bind(this);
+
+    this.state = {
+      inputText: '',
+      markdown: ''
+    }
+
+   
+  }
+
+  onInputChange(e) {
+    this.setState({inputText: e});
+    console.log("Changed state to: " + this.state.inputText);
+  }
+
+
+  render() {
+    return(
+      <div className="App">
+        <InputWindow value={this.state.inputText} onInputChange={this.onInputChange} />
+        <OutputWindow raw_markdown={this.state.inputText}/>
+      </div>
+    )
+  }
 }
+
 
 export default App;
---
+++ src/OutputWindow.js
@@ -0,0 +1,26 @@
+
+
+
+
+import React from 'react'
+import './OutputWindow.css';
+
+const ReactMarkdown = require('react-markdown');
+
+function OutputWindow(props) {
+  const raw_markdown = props.raw_markdown;
+
+  return (
+    <div class="output-window">
+      <p>
+        <ReactMarkdown source={raw_markdown} />
+      </p>
+    </div>
+  )
+}
+
+
+
+
+
+export default OutputWindow;
