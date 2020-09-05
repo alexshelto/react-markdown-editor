1f73835f2385a247f16ce5f30e268e2bc33b3c5f
alexshelto <alexandershelton@icloud.com>
2020-09-04 22:32:46 -0400

    rough dl for file. need to add event handler

--- src/App.js
+++ src/App.js
@@ -16,10 +16,11 @@ class App extends React.Component {
     super(props);
 
     this.onInputChange = this.onInputChange.bind(this);
-
+    this.downloadFile = this.downloadFile.bind(this);
     this.state = {
       inputText: '',
-      markdown: ''
+      markdown: '',
+      fileName: ''
     }
 
 
@@ -28,6 +29,23 @@ class App extends React.Component {
   onInputChange(e) {
     this.setState({ inputText: e });
     console.log("Changed state to: " + this.state.inputText);
+  }
+
+  onFileNameChange(e){
+    this.setState({fileName: e.target.value});
+  }
+
+  downloadFile(filename, text) {
+    var element = document.createElement('a');
+    element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+    element.setAttribute('download', filename);
+
+    element.style.display = 'none';
+    document.body.appendChild(element);
+
+    element.click();
+
+    document.body.removeChild(element);
   }
 
 
@@ -39,6 +57,14 @@ class App extends React.Component {
           <InputWindow value={this.state.inputText} onInputChange={this.onInputChange} />
         </div>
         <div class="right-container">
+
+          <div className="download-box">
+          <form onsubmit={this.downloadFile(this.state.fileName, this.state.inputText)}>
+            <input type="text" name="name" value="file.md" onChange={(e) => { e.preventDefault(); this.onFileNameChange(e.target.value) }} />
+            <input type="submit" value="Download" />
+          </form>
+
+          </div>
           <h4>Markdown</h4>
           <OutputWindow raw_markdown={this.state.inputText} />
         </div>
